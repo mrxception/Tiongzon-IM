a521577f47e6ccc2019d0e5283c18120e9f298a2
LAB13_USER ON WS37 <L13X16W37@LabHCI.net>
2025-09-24 16:50:31 +0800

    feature(app_name): setup templates

--- TiongzonIM/TiongzonIM/settings.py
+++ TiongzonIM/TiongzonIM/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'TiongzonIM.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates/app_name"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
